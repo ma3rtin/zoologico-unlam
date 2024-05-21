4de9ba25a5d701ab3664527fa368c47ab243f626
maertin <mutuverria00gmail.com>
2024-05-21 17:50:53 -0300

    primer commit

---
+++ test/ar/edu/unlam/pb2/test/ZoologicoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZoologicoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
